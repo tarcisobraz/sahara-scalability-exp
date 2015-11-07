b51b9bbb25e5655a058c1cc15a16a15ca083f311
Tarciso Braz <tarcisocomp@gmail.com>
2015-11-07 02:12:20 -0300

    Changed createCluster.py script to create and configure cluster for VocTree experiment

--- createCluster.py
+++ createCluster.py
@@ -9,10 +9,19 @@ import subprocess
 import time
 import getpass
 
-MIN_NUM_ARGS = 4
+MIN_NUM_ARGS = 3
+
+def configureInstances(instancesIps, publicKeyPairPath, privateKeyPairPath):
+    print "Configuring Instances..."
+    for instanceIp in instancesIps:
+            commandArray = ["./SSHWithoutPassword.sh", instanceIp , publicKeyPairPath, privateKeyPairPath]
+            command = ' '.join(commandArray)
+            print command
+            subprocess.call(command,shell=True)
+            print "Configured instance with IP:", instanceIp
 
 def printUsage():
-    print "python create_cluster.py <cluster_name> <key_pair> <config_file_path>"
+    print "python create_cluster.py <cluster_name> <config_file_path>"
 
 if (len(sys.argv) < MIN_NUM_ARGS):
     print "Wrong number of arguments"
@@ -20,8 +29,7 @@ if (len(sys.argv) < MIN_NUM_ARGS):
     exit(1)
 
 cluster_name = sys.argv[1]
-key_pair = sys.argv[2]
-config_file_path = sys.argv[3]
+config_file_path = sys.argv[2]
 
 user = raw_input('OpenStack User: ')
 key = getpass.getpass(prompt='OpenStack Password: ')
@@ -30,6 +38,12 @@ json_parser = JsonParser(config_file_path)
 project_name = json_parser.get('project_name')
 project_id = json_parser.get('project_id')
 main_ip = json_parser.get('main_ip')
+image_id = json_parser.get('image_id')
+net_id = json_parser.get('net_id')
+template_id = json_parser.get('cluster_template_id')
+key_pair = json_parser.get('keypair_name')
+public_keypair_path = json_parser.get('public_keypair_path')
+private_keypair_path = json_parser.get('private_keypair_path')
 connector = ConnectionGetter(user, key, project_name, project_id, main_ip)
 
 keystone_util = UtilKeystone(connector.keystone())
@@ -37,5 +51,9 @@ token_ref_id = keystone_util.getTokenRef(user, key, project_name).id
 sahara_util = UtilSahara(connector.sahara(token_ref_id))
 
 cluster_id = sahara_util.createClusterHadoop(cluster_name, image_id, template_id, net_id,key_pair)
+#cluster_id = "e5b33c1a-a6af-430d-bc0d-531c982f8017"
 print cluster_id
+instancesIps = sahara_util.get_instances_ips(cluster_id)
+configureInstances(instancesIps,public_keypair_path,private_keypair_path)
+master_ip = sahara_util.get_master_ip(cluster_id)
 
